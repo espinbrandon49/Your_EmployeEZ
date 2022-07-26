bd0998a06cebbaa87c06a01d7b5b62f22ae6d95a
Brandon Espinosa <espinbrandon49@gmail.com>
2022-07-25 20:37:04 -0700

    populate seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE roles (
   id INT AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
   department_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
@@ -19,11 +19,11 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
+  manager_id INT DEFAULT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
   ON DELETE SET NULL
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name) 
+VALUES
+('Finance'), ('Sales'),('Engineering'), ('Legal');
+SELECT * FROM department;
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Finance Analyst', 50000, 1), ('Accountant', 85000, 1), ('Salesperson', 40000, 2), ('Account Manager', 75000, 2), ('Software Engineer', 75000, 3),('Lead Engineer', 100000, 3),('Legal Analyst', 50000, 4),('Lawyer', 100000, 4);
+SELECT * FROM roles;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('George', 'Washington', 1, 2),
+('John', 'Adams', 2, NULL),
+('Thomas', 'Jefferson', 3, 4),
+('James', 'Madison', 4, NULL),
+('John', 'Quincyadams', 5, 6),
+('Andrew', 'Jackson', 6, NULL),
+('Martin', 'Vanburen', 7, 8),
+('Martin', 'Vanburen', 8, NULL),
+('William', 'Harrison', 5, 6);
+SELECT * FROM employee;
+
+-- DEFAULT NULL??
